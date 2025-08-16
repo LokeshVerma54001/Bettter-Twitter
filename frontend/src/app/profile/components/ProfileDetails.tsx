4da3a43075d97674c064cdbd783ba8c2760f0b25
LokeshVerma54001 <148640056+LokeshVerma54001@users.noreply.github.com>
2025-08-17 00:42:20 +0530

    profile page frontend skeleton done

--- frontend/src/app/profile/components/ProfileDetails.tsx
+++ frontend/src/app/profile/components/ProfileDetails.tsx
@@ -1,11 +1,78 @@
+import PostCard from '@/components/PostCard'
+import { Calendar, Check, CircleCheckBig } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
+
+const tabs = [{name:"Posts"}, {name:"Replies"}, {name:"Highlights"}, {name:"Articles"}, {name:"Media"}, {name:"Likes"}]
 
 const ProfileDetails = () => {
   return (
-    <div className=' flex flex-col'>
-        <div className=' h-120 border-b border-gray-600'>
-            
+    <div className="flex flex-col">
+      {/* Banner wrapper */}
+      <div className="relative h-60">
+        {/* Banner image */}
+        <Image
+          src="/1080x360.jpg"
+          alt="banner"
+          fill
+          className="object-cover"
+        />
+
+        {/* Profile picture */}
+        <div className="absolute -bottom-16 left-5 rounded-full border-4 border-black">
+          <Image
+            src="/pfp.jpg"
+            alt="pfp"
+            width={126}  // 96px (6rem) for a good size
+            height={126}
+            className="rounded-full"
+          />
         </div>
+      </div>
+      <div className=' flex flex-col h-66 '>
+        <button className=' self-end bg-white text-black mt-5 mr-5 px-7 py-2 rounded-full'>Edit</button>
+        <div className=' flex ml-5 mt-3 items-center gap-3'>
+          <h1 className=' font-bold text-2xl'>User Name</h1>
+          <button className=' flex border rounded-full py-0.5 px-2 text-sm items-center'>
+            <CircleCheckBig className=' text-sky-500 ' size={20}/> Get Verified
+          </button>
+        </div>
+        <p className=' font-light text-sm ml-5 text-gray-500'>@user_name</p>
+        <p className=' mt-3 ml-5 '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam nisi veritatis.</p>
+        <p className=' flex items-center text-gray-500 ml-5 mt-1 gap-2'>
+          <Calendar /> Joined May 7 2025
+        </p>
+        <div className=' flex ml-5 mt-1 gap-5'>
+          <div className=' flex gap-1'>
+            <h1 className=' font-bold'>34</h1>
+            <p className=' text-gray-500'>Following</p>
+          </div>
+          <div className='flex gap-1'>
+            <h2 className=' font-bold'>200</h2>
+            <p className=' text-gray-500'>Followers</p>
+          </div>
+        </div>
+        {/* tabs */}
+        <div className=' flex'>
+          {tabs.map((tab, index) =>(
+            <div
+              key={index}
+              className=' w-[16.6%] flex items-center justify-center h-10 mt-2.5 hover:bg-gray-900 border-b border-gray-600'
+            >
+              {tab.name}
+            </div>
+          ))}
+        </div>
+        {/* posts stuff below */}
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
     </div>
   )
 }
--- frontend/src/app/profile/components/ProfileMain.tsx
+++ frontend/src/app/profile/components/ProfileMain.tsx
@@ -1,14 +1,16 @@
+"use client"
+
 import { Search } from 'lucide-react'
 import React from 'react'
 import ProfileDetails from './ProfileDetails'
 
-const ProfileMain = () => {
+const ProfileMain = () => {1
   return (
     <div className='lg:w-[50%] w-[80%] flex flex-col border-r border-l border-gray-600 lg:ml-[22rem] ml-[10rem] '>
         <div className='flex w-full h-16 justify-between items-center border-b border-gray-600 sticky top-0 bg-black z-10'>
             <div className=' ml-5'>
                 <h1 className=' font-extrabold text-2xl'>User name</h1>
-                <p className=' font-light text-gray-400 text-sm'>36 posts</p>
+                <p className=' font-light text-gray-500 text-sm'>36 posts</p>
             </div>
             <Search className=' mr-5' />
         </div>
